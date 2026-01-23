5a3becaae48dddd07fb1431781d79a0bd05a659a
lucasblld <lucasbillaud2@gmail.com>
2026-01-23 09:23:12 +0400

    add comparison with solution

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import streamlit as st
 SOLUTION_PATH = "answer"
 con = duckdb.connect(database="data/exercises_sql.duckdb", read_only=False)
 
-# solution = duckdb.sql(ANSWER_STR).df()
-
 # SIDE BAR
 with st.sidebar:
     theme = st.selectbox(
@@ -19,6 +17,18 @@ with st.sidebar:
     exersice = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exersice)
 
+    try:
+        exercise_name = exersice.loc[0, "exercise_name"]
+        with open(
+            f"{SOLUTION_PATH}/{exercise_name}.sql", "r", encoding="utf-8"
+        ) as solution_file:
+            answer = solution_file.read()
+
+        SOLUTION = con.execute(answer).df()
+    except (AttributeError, KeyError) as e:
+        print(f"{e} no exercise selected")
+        SOLUTION = None
+
 st.header("enter your code: ")
 st.write("""Spaced repetition system SQL practice""")
 
@@ -27,20 +37,20 @@ query = st.text_area(label="votre sql ici", key="user_input")
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
-#
-#    if len(result.columns) != len(solution.columns):
-#        st.write("some columns are missing")
-#
-#    n_lines_diff = result.shape[0] - solution.shape[0]
-#    if n_lines_diff != 0:
-#        st.write(f"the are a difference of {n_lines_diff} lines")
-#
-#    try:
-#        result = result[solution.columns]
-#        st.dataframe(result.compare(solution))
-#    except KeyError as e:
-#        st.write("Some columns are missing")
-#
+
+    if len(result.columns) != len(SOLUTION.columns):
+        st.write("some columns are missing")
+
+    n_lines_diff = result.shape[0] - SOLUTION.shape[0]
+    if n_lines_diff != 0:
+        st.write(f"the are a difference of {n_lines_diff} lines")
+
+    try:
+        result = result[SOLUTION.columns]
+        st.dataframe(result.compare(SOLUTION))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 #
 with tab2:
@@ -51,16 +61,11 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exersice.loc[0, "exercise_name"]
-    with open(
-        f"{SOLUTION_PATH}/{exercise_name}.sql", "r", encoding="utf-8"
-    ) as solution_file:
-        sql = solution_file.read()
-        st.text(sql)
-        solution_table = con.execute(sql).df()
-        st.dataframe(solution_table)
+    if query:
+        st.text(answer)
+        st.dataframe(SOLUTION)
 
-#    st.write("query solution")
+#    st.write("query SOLUTION")
 #    st.write(ANSWER_STR)
 #    st.write("result")
-#    st.dataframe(solution)
+#    st.dataframe(SOLUTION)
